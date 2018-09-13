b448ade64643eb93b834aac2b6bb20a49c238ece
Dan Milliron <dan@railsblazer.com>
2018-09-13 04:09:47 +0000

    Add the users#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      session[:user_name] = @user.name
+      redirect_to users_path
+    else
+      flash.now[:error] = @user.errors.full_messages
+      render action: :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,4 +7,26 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       assert_response :success
     end
   end
+
+  describe "create" do
+    let(:user_name) { 'some name' }
+    let(:params)    { {user: {name: user_name}} }
+
+    describe 'with valid params' do
+      it 'redirects to the user index page' do
+        post users_url, params: params
+        assert_redirected_to users_path
+      end
+    end
+
+    describe 'with invalid params' do
+      let(:user_name) { nil }
+      it 'renders the sign-in page with a display of the errors' do
+        post users_url, params: params
+        assert_response :success
+        assert_template :new
+        expect(response.body).must_match /Errors<\/h5>/
+      end
+    end
+  end
 end

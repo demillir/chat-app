71aa6f297d038728855de5183756ab62089cb738
Dan Milliron <dan@railsblazer.com>
2018-09-13 17:42:58 +0000

    Switch client code to use ActiveUserDB.[]

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,7 @@
 class ChatsController < ApplicationController
   def show
     partner_name = CGI.unescape(params[:id])
-    partner      = ActiveUserDB.instance.find { |user| user.name == partner_name }
+    partner      = ActiveUserDB.instance[partner_name]
     @chat        = Chat.new(initiator: current_user, partner: partner)
   end
 end
